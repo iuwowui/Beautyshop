f2f1d136aaae0d7dbe3da598f9271bd23c6a1853
ljr <709113980@qq.com>
2019-06-04 21:09:05 +0800

    info

--- src/main/java/com/aimei/beautyshop/BeautyshopApplication.java
+++ src/main/java/com/aimei/beautyshop/BeautyshopApplication.java
@@ -1,8 +1,10 @@
 package com.aimei.beautyshop;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.aimei.beautyshop.dao")
 @SpringBootApplication
 public class BeautyshopApplication {
 
