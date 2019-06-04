631c890ac1c579fe82cd8a3f47eb0e52bf1b0d35
ljr <709113980@qq.com>
2019-06-04 20:22:32 +0800

    add JsonBean

--- src/main/java/com/aimei/beautyshop/vo/JsonBean.java
+++ src/main/java/com/aimei/beautyshop/vo/JsonBean.java
@@ -1,4 +1,32 @@
 package com.aimei.beautyshop.vo;
 
+import lombok.Data;
+
+@Data
 public class JsonBean {
+    private int code;
+    private String msg;
+    private Object data;
+
+    public static JsonBean setOK(){
+        JsonBean bean=new JsonBean();
+        bean.setCode(1);
+        bean.setMsg("OK");
+        bean.setData(null);
+        return bean;
+    }
+    public static JsonBean setOK(String msg,Object data){
+        JsonBean bean=new JsonBean();
+        bean.setCode(1);
+        bean.setMsg(msg);
+        bean.setData(data);
+        return bean;
+    }
+    public static JsonBean setERROR(){
+        JsonBean bean=new JsonBean();
+        bean.setCode(0);
+        bean.setMsg("ERROR");
+        bean.setData(null);
+        return bean;
+    }
 }
