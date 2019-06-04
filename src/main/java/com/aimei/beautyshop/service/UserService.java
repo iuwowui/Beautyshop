e2672f396f069b71777d10408dbba841682ee6a3
ljr <709113980@qq.com>
2019-06-04 22:39:43 +0800

    userLogin

--- src/main/java/com/aimei/beautyshop/service/UserService.java
+++ src/main/java/com/aimei/beautyshop/service/UserService.java
@@ -1,4 +1,7 @@
 package com.aimei.beautyshop.service;
 
+import com.aimei.beautyshop.entity.User;
+
 public interface UserService {
+    User login(int id);
 }
--- src/main/java/com/aimei/beautyshop/service/impl/UserServiceImpl.java
+++ src/main/java/com/aimei/beautyshop/service/impl/UserServiceImpl.java
@@ -1,4 +1,23 @@
 package com.aimei.beautyshop.service.impl;
 
-public class UserServiceImpl {
+import com.aimei.beautyshop.dao.UserMapper;
+import com.aimei.beautyshop.entity.User;
+import com.aimei.beautyshop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+    @Override
+    public User login(int id) {
+        User user = userMapper.selectById(id);
+        if (user == null){
+            throw new RuntimeException("该用户不存在");
+        }
+
+        return null;
+    }
 }
