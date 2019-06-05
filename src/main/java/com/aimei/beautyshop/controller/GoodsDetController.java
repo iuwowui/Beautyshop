25554ca0cf9810cf059555982118685d7d0186c8
Demos <1147016788@qq.com>
2019-06-05 19:02:09 +0800

    19:01

--- src/main/java/com/aimei/beautyshop/controller/GoodsDetController.java
+++ src/main/java/com/aimei/beautyshop/controller/GoodsDetController.java
@@ -6,7 +6,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -18,10 +20,12 @@ public class GoodsDetController {
     private GoodsDetService goodsDetService;
 
 
+    @CrossOrigin
     @ApiOperation(value = "通过商品id查看该商品的详情", notes = "查看商品详情")
-    @RequestMapping("")
+    @RequestMapping(value = "/goodsDet/findGoodsDet.do",method = RequestMethod.GET)
     public JsonBean findGoodsDet(int goodsId){
         JsonBean bean = goodsDetService.findGoodsDetByGid(goodsId);
         return bean;
     }
+
 }
