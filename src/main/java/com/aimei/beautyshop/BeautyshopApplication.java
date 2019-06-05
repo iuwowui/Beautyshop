d01d00e4247e8c3cb5aae5426edc65940d057fbd
Demos <1147016788@qq.com>
2019-06-05 21:56:10 +0800

    21:55 事务开启

--- src/main/java/com/aimei/beautyshop/BeautyshopApplication.java
+++ src/main/java/com/aimei/beautyshop/BeautyshopApplication.java
@@ -3,8 +3,10 @@ package com.aimei.beautyshop;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableTransactionManagement
 @MapperScan("com.aimei.beautyshop.dao")
 @SpringBootApplication
 @EnableSwagger2
--- src/main/java/com/aimei/beautyshop/service/impl/CartServiceImpl.java
+++ src/main/java/com/aimei/beautyshop/service/impl/CartServiceImpl.java
@@ -7,10 +7,12 @@ import com.aimei.beautyshop.vo.JsonBean;
 import com.aimei.beautyshop.vo.VCart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class CartServiceImpl implements CartService {
 
 
