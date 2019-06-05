7eb42da8981b18fc9cc640b11a3f692fc0063880
Demos <1147016788@qq.com>
2019-06-05 09:44:24 +0800

    配置mapper

--- src/main/java/com/aimei/beautyshop/config/SwaggerConfig.java
+++ src/main/java/com/aimei/beautyshop/config/SwaggerConfig.java
@@ -1,7 +1,30 @@
 package com.aimei.beautyshop.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
 /**
  * Swagger设置
  */
+@Configuration
 public class SwaggerConfig {
+
+    public ApiInfo createA(){
+        ApiInfo info=new ApiInfoBuilder().title("美妆小店微信小程序接口").
+                contact( new Contact("美妆小店开发团","http://1000phone.com","oldtimes@163.com")).
+                description("实现一个新闻项目的数据接口信息").build();
+        return info;
+    }
+    @Bean//创建对象  修饰方法 方法的返回值必须是引用类型  对象存储在IOC容器
+    public Docket createDocket(){
+        Docket docket=new Docket(DocumentationType.SWAGGER_2).apiInfo(createA()).select().
+                apis(RequestHandlerSelectors.basePackage("com.qfedu.springbot.controller")).build();
+        return docket;
+    }
 }
