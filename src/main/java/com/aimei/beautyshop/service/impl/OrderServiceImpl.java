27dba727507a96893f42729b19cc8c4c8c58d01c
Demos <1147016788@qq.com>
2019-06-11 11:02:00 +0800

    2019 06 -11 商品查询完善

--- src/main/java/com/aimei/beautyshop/service/impl/OrderServiceImpl.java
+++ src/main/java/com/aimei/beautyshop/service/impl/OrderServiceImpl.java
@@ -58,9 +58,7 @@ public class OrderServiceImpl implements OrderService {
         SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
         String time = format.format(new Date());
         String str = "";
-        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-                'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-                'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
         Random random = new Random();
         for (int i = 0; i <3; i++){
             char num = ch[random.nextInt(ch.length)];
